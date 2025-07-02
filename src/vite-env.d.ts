73243aaaeef951a0e100d9fa90620501f902b515
Rainy-Mountain <irene1618@naver.com>
2025-07-02 14:11:01 +0900

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+// 이미지 파일
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+
+// 스타일 파일
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
