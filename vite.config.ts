a9c4a86ea346606ea2a62832e005bbe50141e7ec
Rainy-Mountain <irene1618@naver.com>
2025-09-29 20:30:37 +0900

    Revert "Fix: MIME 타입 오류 완전 해결"
    This reverts commit fb3d2a433db9b934d6862e5ffad0a8b751b0cbde.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
+  server: {
+    hmr: {
+      port: 443,
+    },
+  },
 })
